9a6c19fd7b151bc0b56d9f01f85d73f3af202dcd
avishkararjan <avishkararjan@gmail.com>
2024-05-21 16:32:04 +0530

    vit_cifar10 - NewGELU activation

--- vision_transformer/model.py
+++ vision_transformer/model.py
@@ -8,171 +8,191 @@ Also : https://tintn.github.io/Implementing-Vision-Transformer-from-Scratch/"""
 import torch
 import torch.nn as nn
 
-class PatchEmbed(nn.Module):
-  def __init__(self, img_size, patch_size, in_channel=3, embed_dim=768):
-    super().__init__()
-    self.img_size = img_size
-    self.patch_size = patch_size
-    self.n_patches = (img_size//patch_size)**2
+"""MODEL"""
 
-    self.proj = nn.Conv2d(
-      in_channel,
-      embed_dim,
-      kernel_size=patch_size,
-      stride=patch_size
-    )
-  def forward(self, x):
-    x = self.proj(x)
-    x = x.flatten(2)
-    x = x.transpose(1,2)
-
-    return x
-
-class Attention(nn.Module):
-  def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
-    super().__init__()
-    self.n_heads=n_heads
-    self.dim=dim
-    self.head_dim= dim//n_heads
-    self.scale=self.head_dim** -0.5
-
-    self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
-    self.attn_drop = nn.Dropout(attn_p)
-    self.proj = nn.Linear(dim, dim)
-    self.proj_drop = nn.Dropout(proj_p)
-
-  def forward(self, x):
-    batch, n_tokens, dim = x.shape
-    # assert dim != self.dim , "chud gaya"
-    if dim != self.dim:
-      raise ValueError
+import torch.nn as nn
+import torch.nn.functional as F
+import math
 
 
-    qkv = self.qkv(x)
-    qkv = qkv.reshape(batch, n_tokens, 3, self.n_heads, self.head_dim)
-    qkv = qkv.permute(2,0,3,1,4)
-    q,k,v = qkv[0], qkv[1], qkv[2]
-    k_t = k.transpose(-2,-1)
-    dp = (
-      q@k_t
-    )*self.scale
-    attn = dp.softmax(dim=-1)
-    attn = self.attn_drop(attn)
-    weighted_avg = attn@v
-    weighted_avg = weighted_avg.transpose(1,2)
-    weighted_avg = weighted_avg.flatten(2)
+class PatchEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.img_size = config["img_size"]
+        self.num_channels = config["num_channels"]
+        self.patch_size = config["patch_size"]
+        self.embed_dim = config["embed_dim"]
+        
+        self.num_patches = (self.img_size//self.patch_size)**2
+        self.projection = nn.Conv2d(self.num_channels, self.embed_dim, kernel_size=self.patch_size, stride=self.patch_size)
 
-    x = self.proj(weighted_avg)
-    x = self.proj_drop(x)
+    def forward(self, x):
+        x = self.projection(x)
+        # print(x.shape)
+        x = x.flatten(2).transpose(1,2)
+        return x
 
-    return x
+class Embeddings(nn.Module):
+    # Patch Embeddings + (CLS Token + Positional Embeddings )
+    def __init__(self, config):
+        super().__init__()
+        self.patch_embeddings = PatchEmbeddings(config)
+        self.embed_dim = config["embed_dim"]
+        self.cls_token = nn.Parameter(torch.randn(1,1,self.embed_dim))
+        self.positional_embeddings = nn.Parameter(torch.randn(1, self.patch_embeddings.num_patches+1, self.embed_dim))
+        self.dropout = nn.Dropout(config["dropout"])
 
+    def forward(self, x):
+        x = self.patch_embeddings(x)
+        batch_size, _, _ = x.size()
+        cls_token = self.cls_token.expand(batch_size, -1, -1) # (1, 1, hidden_size) -> (batch_size, 1, hidden_size)
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.positional_embeddings
+        x = self.dropout(x)
+        return x
+
+class AttentionHead(nn.Module):
+    def __init__(self, attention_head_size,config):
+        super().__init__()
+        self.embed_dim = config["embed_dim"]
+        self.attention_head_size = attention_head_size
+        self.bias = config["bias"]
+        
+        self.query = nn.Linear(self.embed_dim, self.attention_head_size, bias=self.bias)
+        self.key = nn.Linear(self.embed_dim, self.attention_head_size, bias=self.bias)
+        self.value = nn.Linear(self.embed_dim, self.attention_head_size, bias=self.bias)
+
+        self.dropout = nn.Dropout(config["dropout"])
+
+    def forward(self, x):
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
+
+        attention_scores = torch.matmul(q, k.transpose(-1,-2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = nn.functional.softmax(attention_scores, dim=-1)
+        attention_scores = self.dropout(attention_scores)
+        attention_out = torch.matmul(attention_scores, v)
+        
+        return attention_out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embed_dim = config["embed_dim"]
+        self.num_heads = config["num_heads"]
+        self.head_size = self.embed_dim//self.num_heads
+        self.all_head_size = self.head_size * self.num_heads
+        self.dropout = config["dropout"]
+        self.qkv_bias = config["bias"]
+
+        self.heads = nn.ModuleList([
+            AttentionHead(
+                self.head_size,
+                config
+            ) for _ in range(self.num_heads)
+        ])
+
+        self.attention_mlp = nn.Linear(self.all_head_size, self.embed_dim)
+        self.out_dropout = nn.Dropout(config["dropout"])
+
+    def forward(self, x):
+        attention_outputs = [head(x) for head in self.heads]
+        attention_output = torch.cat([attention_output for attention_output in attention_outputs], dim=-1) #concat attention for each head
+        attention_output = self.attention_mlp(attention_output)
+        attention_output = self.out_dropout(attention_output)
+
+        return attention_output
+        
 class MLP(nn.Module):
-  def __init__(self, in_features, hidden_features, out_features, p=0.):
-    super().__init__()
-    self.fc1 = nn.Linear(in_features, hidden_features)
-    self.act = nn.GELU()
-    self.fc2 = nn.Linear(hidden_features, out_features)
-    self.drop = nn.Dropout(p)
+    def __init__(self, config):
+        super().__init__()
+        self.embed_dim = config["embed_dim"]
+        self.hidden_dim = config["hidden_dim"]
+        self.fc1 = nn.Linear(self.embed_dim, self.hidden_dim)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(self.hidden_dim, self.embed_dim)
+        self.dropout=nn.Dropout(config["dropout"])
+
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embed_dim=config["embed_dim"]
+        self.num_heads = config["num_heads"]
+        self.hidden_dim = config["hidden_dim"]
+        self.attention = MultiHeadAttention(config)
+        self.layer_norm1 = nn.LayerNorm(self.embed_dim)
+        self.mlp = MLP(config)
+        self.layer_norm2 = nn.LayerNorm(self.embed_dim)
+
+    def forward(self,x):
+        attention_output = self.attention(self.layer_norm1(x))
+        x = x+attention_output
+        mlp_out = self.mlp(self.layer_norm2(x))
+        x = x+mlp_out
+        return x
+        
+class Encoder(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        self.blocks = nn.ModuleList([Block(config) for _ in range(config["num_hidden_layers"])])
+    def forward(self,x ):
+        all_attentions = []
+        for block in self.blocks:
+            x = block(x)
+        return x
+
+class ViT_101(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.img_size = config["img_size"]
+        self.embed_dim = config["embed_dim"]
+        self.num_classes = config["num_classes"]
+    
+        self.embeddings = Embeddings(config)
+        self.encoder = Encoder(config)
+    
+        self.classifier = nn.Linear(self.embed_dim, self.num_classes)
+
+        self.apply(self._init_weights)
+
+    def forward(self, x):
+        embedding_output = self.embeddings(x)
+        encoder_output = self.encoder(embedding_output)
+        classification = self.classifier(encoder_output[:, 0, :])
+        return classification
 
 
-  def forward(self, x):
-    x = self.fc1(x)
-    x= self.act(x)
-    x = self.drop(x)
-    x = self.fc2(x)
-    x= self.drop(x)
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Conv2d)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        elif isinstance(module, Embeddings):
+            module.positional_embeddings.data = nn.init.trunc_normal_(
+                module.positional_embeddings.data.to(torch.float32),
+                mean=0.0,
+                std=0.02,
+            ).to(module.positional_embeddings.dtype)
 
-    return x
+            module.cls_token.data = nn.init.trunc_normal_(
+                module.cls_token.data.to(torch.float32),
+                mean=0.0,
+                std=0.02,
+            ).to(module.cls_token.dtype)
 
-class TransformerBlock(nn.Module):
-  def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0, attn_p=0.):
-    super().__init__()
-    self.norm1 = nn.LayerNorm(dim, eps=1e-6)
-    self.attn = Attention(
-      dim,
-      n_heads=n_heads,
-      qkv_bias=qkv_bias,
-      attn_p=attn_p,
-      proj_p=p
-    )
-    self.norm2 = nn.LayerNorm(dim, eps=1e-6)
-    hidden_features = int(dim*mlp_ratio)
-    self.mlp = MLP(
-      in_features=dim,
-      hidden_features=hidden_features,
-      out_features=dim
-    )
-
-  def forward(self,x):
-    x = x + self.attn(self.norm1(x))
-    x = x + self.mlp(self.norm2(x))
-
-    return x
-
-class VisionTransformer(nn.Module):
-  def __init__(
-    self,
-    img_size=224,
-    patch_size=16,
-    in_channels=3,
-    n_classes=1000,
-    embed_dim=768,
-    depth=12,
-    n_heads=12,
-    mlp_ratio=4,
-    qkv_bias=True,
-    p=0.,
-    attn_p=0.,
-  ):
-    super().__init__()
-
-    self.patch_embed = PatchEmbed(
-      img_size=img_size,
-      patch_size=patch_size,
-      in_channel=in_channels,
-      embed_dim=embed_dim
-    )
-    self.cls_token = nn.Parameter(torch.zeros(1,1, embed_dim))
-    self.pos_embed = nn.Parameter(
-      torch.zeros(1,1+self.patch_embed.n_patches, embed_dim)
-    )
-    self.pos_drop = nn.Dropout(p=p)
-
-    self.blocks = nn.ModuleList(
-      [
-        TransformerBlock(dim=embed_dim,
-             n_heads=n_heads,
-             mlp_ratio=mlp_ratio,
-             qkv_bias=qkv_bias,
-             p=p,
-             attn_p=attn_p
-             )
-        for _ in range(depth)
-      ]
-    )
-
-    self.norm = nn.LayerNorm(embed_dim, eps=1e-6)
-    self.head = nn.Linear(embed_dim, n_classes)
-
-  def forward(self, x):
-    batch_size = x.shape[0]
-    x = self.patch_embed(x)
-    cls_token = self.cls_token.expand(batch_size, -1,-1) # (batch, 1, embed_dim)
-    x = torch.cat((cls_token, x), dim=1)
-    x = x + self.pos_embed
-    x = self.pos_drop(x)
-
-
-    for block in self.blocks:
-      x= block(x)
-
-
-    x = self.norm(x)
-    cls_token_final = x[:, 0]# just cls token
-    x = self.head(cls_token_final)
-
-    return x
+          
 
 
 """
